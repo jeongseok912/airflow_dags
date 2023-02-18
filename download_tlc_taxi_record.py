db889593369021f403bbaf66811ab71a6da2dea7
bradley <bradley@LAPTOP-QSKLTQDD.localdomain>
2023-02-18 16:06:53 +0900

    Update download_tlc_taxi_record.py

--- download_tlc_taxi_record.py
+++ download_tlc_taxi_record.py
@@ -5,8 +5,14 @@ import logging
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.providers.mysql.operators.mysql import MySqlOperator, MySqlHook
+from airflow.providers.mysql.operators.mysql import MySqlOperator
+from airflow.providers.mysql.hooks.mysql import MySqlHook
 from airflow.models import Variable
+
+
+get_latest_dataset_link_sql = """
+    SELECT DATE_SUB(CURDATE(), INTERVAL 1 DAY);
+    """
 
 
 class DBHandler(logging.StreamHandler):
@@ -20,6 +26,10 @@ class DBHandler(logging.StreamHandler):
         if record:
             self.cursor.execute(
                 f"INSERT INTO log VALUES ('{record.msg}', SYSDATE());")
+
+    def select(self):
+        results = self.cursor.execute(get_latest_dataset_link_sql)
+        print(results)
 
     def close(self):
         self.conn.commit()
@@ -66,35 +76,7 @@ def download_dataset_and_upload_to_s3(year, month, day, hour, minute, utc_dt, ut
     logger.info(url)
 
     dbhandler.close()
-    '''
-    print(context['logical_date'].strftime('%Y-%m-%d'))
-    ts = context["logical_date"].timestamp()
-    print(int(ts))
 
-    year_month = []
-    for i in [2019, 2020, 2021, 2022]:
-        for j in range(1, 13):
-            month = f"0{j}"[-2:]
-            year_month.append(f"{i}-{month}")
-
-    # mongodb metafh 변경
-    year_month = year_month[1:]  # 2019년 2월부터 FHVHV 데이터 존재
-
-    # print("******************************************")
-    # print("selected dataset: ", get_meta_log())
-
-    i = 0  # DB에 max(created_at)의 index 값 가져오기
-    print(year_month[i])
-    print(context["run_id"])
-
-    # 1675745379 # 4의 ts 근처 배수
-    '''
-    print("----------------------------")
-
-
-get_latest_dataset_link_sql = """
-    SELECT DATE_SUB(CURDATE(), INTERVAL 1 DAY);
-    """
 
 '''
 get_latest_dataset_link_sql = """
@@ -111,14 +93,6 @@ get_latest_dataset_link_sql = """
 '''
 
 
-def get_latest_dataset_link(**context):
-    ti = context['ti']
-    result = ti.xcom_pull(task_ids='get_latest_dataset_id')
-    if result:
-        for row in result:
-            print(row)
-
-
 with DAG(
     'download_tlc_taxi_record',
     start_date=datetime(2022, 2, 6),
@@ -129,12 +103,6 @@ with DAG(
         task_id='get_latest_dataset_id',
         mysql_conn_id='TLC_TAXI',
         sql=get_latest_dataset_link_sql
-    )
-
-    get_latest_dataset_link = PythonOperator(
-        task_id='get_latest_dataset_link',
-        provide_context=True,
-        python_callable=get_latest_dataset_link
     )
 
     download_dataset_and_upload_to_s3 = PythonOperator(
@@ -153,4 +121,4 @@ with DAG(
         provide_context=True
     )
 
-get_latest_dataset_id >> get_latest_dataset_link >> download_dataset_and_upload_to_s3
+get_latest_dataset_id >> download_dataset_and_upload_to_s3
